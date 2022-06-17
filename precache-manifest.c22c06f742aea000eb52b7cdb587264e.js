a17d82995d8efafe1130881430b11dfd1bbf0b64
lucky90354 <luckychauhan7478844@gmail.com>
2022-06-18 00:40:05 +0530

    initial commit

---
+++ precache-manifest.c22c06f742aea000eb52b7cdb587264e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ac04b81e6d2a0977092dd8e19e64c9",
+    "url": "http://ravikesh.tech/shopping/index.html"
+  },
+  {
+    "revision": "82835173c13db74500f3",
+    "url": "http://ravikesh.tech/shopping/static/css/main.74f00d76.chunk.css"
+  },
+  {
+    "revision": "eb7b074b524cb4ba949b",
+    "url": "http://ravikesh.tech/shopping/static/js/2.f501c270.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "http://ravikesh.tech/shopping/static/js/2.f501c270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82835173c13db74500f3",
+    "url": "http://ravikesh.tech/shopping/static/js/main.295ccde2.chunk.js"
+  },
+  {
+    "revision": "d03ea13c71a7767536cb",
+    "url": "http://ravikesh.tech/shopping/static/js/runtime-main.961501e9.js"
+  },
+  {
+    "revision": "f3726bcc68c9c83b2eec846fb3fa24cf",
+    "url": "http://ravikesh.tech/shopping/static/media/header.f3726bcc.jpg"
+  }
+]);
